9cdbda9dbad96e8129672afc54cb61273a0e6ab8
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-07-16 20:32:17 -0400

    Started Chess project

---
+++ ChineseChess.py
@@ -0,0 +1,64 @@
+#!usr/bin/env python 3
+
+# 中国象棋
+
+class Chess:
+    def __init__(self) -> None:
+
+        self.board = [
+            1, 2, 6, 5, 7, 5, 6, 2, 1,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 3, 0, 0, 0, 0, 0, 3, 0,
+            4, 0, 4, 0, 4, 0, 4, 0, 4,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+            11, 0, 11, 0, 11, 0, 11, 0, 11,
+            0, 10, 0, 0, 0, 0, 0, 10, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+            8, 9, 13, 12, 14, 12, 13, 9, 8]
+
+        # Pieces
+        # 車, 馬, 砲, 卒, 士, 象, 將 --- Player 1
+        # 俥, 傌, 炮, 兵, 仕, 相, 帥 --- Player 2
+
+        self.Pieces = {
+            0:"  ",
+            1:"車",
+            2:"馬",
+            3:"砲",
+            4:"卒",
+            5:"士",
+            6:"象",
+            7:"將",
+            8:"俥",
+            9:"傌",
+            10:"炮",
+            11:"兵",
+            12:"仕",
+            13:"相",
+            14:"帥",
+        }
+    
+        self.Player = True # Player 1 to move
+
+    def print_board(self):
+        for i in range(10):
+            for j in range(9):
+                ending = "|" if j != 8 else "| " + chr(abs(i - 9) + 65)
+                print(self.Pieces[self.board[i * 9 + j]], end = ending)
+            print()
+        for i in range(9):
+            print(i + 1, end = "  ")
+
+    def play_move(self, move, board):
+        # play a move, on a test board
+        # make shallow copy of board
+        test_board = board[:]
+        pass
+
+    def notationSq(self, square):
+        return abs((ord(square[0])-65)-10)
+    
+
+Game = Chess()
+Game.print_board()
